45225f1da1ed4bda54d7ad1aaec76a8d7cd7b548
Kapongoboy <plamedi.luis11@gmail.com>
2022-11-21 10:16:08 +0200

    all win conditions added

--- mechanics.py
+++ mechanics.py
@@ -1,4 +1,5 @@
 from clear import clear
+clear()
 
 class TicTacToe:
    def __init__(self) -> None:
@@ -15,18 +16,40 @@ class TicTacToe:
                           }
       self.tile = f"{self.coordinates[7]}|{self.coordinates[8]}|{self.coordinates[9]}\n-----------\n{self.coordinates[4]}|{self.coordinates[5]}|{self.coordinates[6]}\n-----------\n{self.coordinates[1]}|{self.coordinates[2]}|{self.coordinates[3]}"
       self.game_over = False
+      print(self.tile)
 
    def play_game(self):
-      clear()
-      print(self.tile)
       self.coordinates[int(input())] = " X "
       self.tile = f"{self.coordinates[7]}|{self.coordinates[8]}|{self.coordinates[9]}\n-----------\n{self.coordinates[4]}|{self.coordinates[5]}|{self.coordinates[6]}\n-----------\n{self.coordinates[1]}|{self.coordinates[2]}|{self.coordinates[3]}"
+      clear()
+      print(self.tile)
       self.game_over = self.win_condi()
       if not self.game_over:
          self.play_game()
 
    def win_condi(self):
-      if (self.coordinates[7] and self.coordinates[8] and self.coordinates[9]) == " X ":
+      if self.coordinates[7] == " X " and self.coordinates[8] ==" X " and self.coordinates[9] == " X ":
+         print("Congrates X wins")
+         return True
+      elif self.coordinates[4] == " X " and self.coordinates[5] ==" X " and self.coordinates[6] == " X ":
+         print("Congrates X wins")
+         return True
+      elif self.coordinates[1] == " X " and self.coordinates[2] ==" X " and self.coordinates[3] == " X ":
+         print("Congrates X wins")
+         return True
+      elif self.coordinates[7] == " X " and self.coordinates[4] ==" X " and self.coordinates[1] == " X ":
+         print("Congrates X wins")
+         return True
+      elif self.coordinates[8] == " X " and self.coordinates[5] ==" X " and self.coordinates[2] == " X ":
+         print("Congrates X wins")
+         return True
+      elif self.coordinates[9] == " X " and self.coordinates[6] ==" X " and self.coordinates[3] == " X ":
+         print("Congrates X wins")
+         return True
+      elif self.coordinates[7] == " X " and self.coordinates[5] ==" X " and self.coordinates[3] == " X ":
+         print("Congrates X wins")
+         return True
+      elif self.coordinates[9] == " X " and self.coordinates[5] ==" X " and self.coordinates[1] == " X ":
          print("Congrates X wins")
          return True
       else:
